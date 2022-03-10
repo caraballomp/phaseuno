b862e3a2d7fdc275997a67bf0f186b897e26df4f
Michelle Caraballo <caraballomp@yahoo.com>
2022-03-09 21:45:28 -0800

    update js information

--- script.js
+++ script.js
@@ -6,15 +6,38 @@ document.addEventListener("DOMContentLoaded", function() {
 
  const API = 'http://localhost:3000/posts'
 
- fetch(API)
-  .then((res) => res.json())
-  .then(renderDogs)
-  .catch(console.err);
+//  fetch(API, {mode:'no-cors'})
+//   .then((res) => {
+//       console.log(res)
+//       res.json()
+//     console.log(res.json())
+// })
+
+//   .then(renderDogs)
+//   .catch(console.err);
+
+const dogs = async () => {
+    const response = await fetch(API)
+    const json = await response.json()
+    console.log(json)
+    return json;
+}
+// dogs();
+
+const allPosts = async () =>{
+    const data = await dogs()
+    renderDogs(data)
+}
+allPosts();
 
   function renderDogs (data) {
     data.forEach(element => {
         // console.log(element.image)
-    
+    imageElement = document.createElement('div');
+    imageTag = document.createElement('img');
+    imageTag.src = element.image;
+    imageContainer.append(imageElement);
+    imageElement.append(imageTag);
     });
 
   }
